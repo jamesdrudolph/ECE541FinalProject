c1db76c3283fb255ede4d99ab451f49fa16422a8
Eric Peart <elpeart@gmail.com>
2021-04-27 23:17:33 -0400

    started on code for state machine

--- FinalProject/src/FinalProject.vhd
+++ FinalProject/src/FinalProject.vhd
@@ -9,7 +9,7 @@ entity FinalProject is
 		KEY			:	in	std_logic_vector(3 downto 0);
 		SW			:	in	std_logic_vector(9 downto 0);
 		LEDR		:	out	std_logic_vector(9 downto 0)
-	);
+		);
 end entity FinalProject;
 
 architecture arch of FinalProject is
@@ -19,7 +19,7 @@ architecture arch of FinalProject is
 			reset_reset_n 	: in  std_logic                    := 'X'; 				-- reset_n
 			led_export    	: out std_logic_vector(9 downto 0);        				-- export
 			switches_export	: in  std_logic_vector(9 downto 0) := (others => 'X')	-- export
-		);
+			);
 	end component embedded_soc;
 	
 	component IrisDataManager is
@@ -28,11 +28,20 @@ architecture arch of FinalProject is
 			SelectDataType	:	in	std_logic;
 			SelectDataIndex	:	in	unsigned(6 downto 0);
 			SelectDataOut	:	out	DataAttributes
-		);
-	end component IrisDataManager;
+			);
+	end component IrisDataManager; 
 	
-
-
+	component EuclideanDistance is
+		port (
+			CLK			:	in	std_logic;
+			TrainingData:	in	DataAttributes;
+			TestData	:	in	DataAttributes;
+			Distance	:	out	std_logic_vector(7 downto 0)
+			);
+	end component EuclideanDistance;
+	
+	
+	
 	
 	signal LEDR_export	:	std_logic_vector(9 downto 0);
 	
@@ -42,7 +51,11 @@ architecture arch of FinalProject is
 	
 	signal SqrtRadical	:	std_logic_vector(15 downto 0);
 	signal SqrtResult	:	std_logic_vector(7 downto 0);
-	signal SqrtRemainder:	std_logic_vector(8 downto 0);
+	signal SqrtRemainder:	std_logic_vector(8 downto 0);	 
+	
+	signal state, nstate: std_logic_vector(4 downto 0) := "00001"; --size may change	 
+	signal count: integer range 0 to 119 := 0;
+	signal training_data: DataArray;
 begin
 	u0 : component embedded_soc
 	port map (
@@ -50,7 +63,7 @@ begin
 		reset_reset_n 		=> KEY(0),		-- reset.reset_n
 		led_export    		=> LEDR_export,	-- led.export
 		switches_export 	=> SW			-- switches.export
-	);
+		);
 	
 	u1 : component IrisDataManager
 	port map (
@@ -58,5 +71,9 @@ begin
 		SelectDataType		=> IrisDataType,
 		SelectDataIndex		=> IrisIndex,
 		SelectDataOut		=> IrisDataOut
-	);
+		);	   
+		
+	
+			
+		
 end architecture arch;
