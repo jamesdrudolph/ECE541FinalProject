d4f694e8e731b413678aee36c39912669cec2974
Eric Peart <elpeart@gmail.com>
2021-04-27 22:21:31 -0400

    constrained integer size in DataAttributes type

--- FinalProject/src/Common.vhd
+++ FinalProject/src/Common.vhd
@@ -2,5 +2,5 @@ library ieee;
 use ieee.numeric_std.all;
 
 package Common is
-	type DataAttributes is array(0 to 4) of integer;
+	type DataAttributes is array(0 to 4) of integer range 0 to 127;
 end Common;
