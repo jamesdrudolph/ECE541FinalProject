cab24268bceaf6aee3069b283c0d26012f3ae936
Eric Peart <elpeart@gmail.com>
2021-05-02 12:24:17 -0400

    added classification output to FinalProject.vhd

--- FinalProject/src/FinalProject.vhd
+++ FinalProject/src/FinalProject.vhd
@@ -8,7 +8,13 @@ entity FinalProject is
 		CLOCK_50	:	in	std_logic;
 		KEY			:	in	std_logic_vector(3 downto 0);
 		SW			:	in	std_logic_vector(9 downto 0);
-		LEDR		:	out	std_logic_vector(9 downto 0)
+		LEDR		:	out	std_logic_vector(9 downto 0);
+		HEX0 		: 	OUT STD_LOGIC_VECTOR(0 to 6);
+		HEX1 		: 	OUT STD_LOGIC_VECTOR(0 to 6);
+		HEX2 		:	OUT STD_LOGIC_VECTOR(0 to 6);
+		HEX3 		: 	OUT STD_LOGIC_VECTOR(0 to 6);
+		HEX4 		: 	OUT STD_LOGIC_VECTOR(0 to 6);
+		HEX5 		: 	OUT STD_LOGIC_VECTOR(0 to 6)
 		);
 end entity FinalProject;
 
@@ -51,6 +57,16 @@ architecture arch of FinalProject is
 			);
 	end component knn_class;
 	
+	component classifier is
+		port (
+			CLK			:	in	std_logic;							   
+			rst			:	in 	std_logic;
+			mins		: 	in	distarr_array; 
+			class		: 	out integer range 0 to 2;
+			done		: 	out std_logic
+			);
+	end component classifier;
+	
 	
 	signal LEDR_export	:	std_logic_vector(9 downto 0);
 	
@@ -65,13 +81,13 @@ architecture arch of FinalProject is
 	signal state, nstate: std_logic_vector(2 downto 0) := "001"; --size may change	 
 	signal count: integer range 0 to 120 := 0;
 	signal training_data: DataArray;	 
-	signal wait_count: unsigned(2 downto 0) := (others => '1'); --may need to adjust depending on how long classification takes	 
-	signal wait_zero: unsigned(wait_count'range) := (others => '0');
+	--signal wait_count: unsigned(2 downto 0) := (others => '1'); --may need to adjust depending on how long classification takes	 
+	--signal wait_zero: unsigned(wait_count'range) := (others => '0');
 	signal test_data: DataAttributes;		 
 	signal classifications: TestClass;	
 	signal knn_out: integer range 0 to 2;	  
-	signal rst_k: std_logic;
-	signal done_k: std_logic;
+	signal rst_k, rst_c: std_logic;
+	signal done_k, done_c: std_logic;
 	signal kData: DataSlice;
 	signal min_dists: distarr(1 to 5);
 	
@@ -106,19 +122,30 @@ begin
 		rst					=> rst_k,
 		min_dists			=> min_dists,
 		done				=> done_k
+		);	   
+		
+	c1: component classifier
+	port map (
+		CLK					=> CLOCK_50,							   
+		rst					=> rst_c,
+		mins				=> mins, 
+		class				=> knn_out,
+		done				=> done_c
 		);
 	
 	offset <= data_offset * offset_count;	
 	s1: for i in 1 to 40 generate
 		kData(i) <= training_data(i - 1 + offset);
 	end generate;
-			  
+	
 	
 	process(CLOCK_50)
-	begin
-		if rising_edge(CLOCK_50) then
+	begin	
+		if KEY(0) = '0' then
+			state <= "001";
+			count <= 0;
+		elsif rising_edge(CLOCK_50) then
 			state <= nstate; 
-			wait_count <= (others => '1');
 			if state(0) = '1' then 	
 				IrisDataType <= '0';  --state 0 loads training data	   
 				if count < 120 then 
@@ -131,30 +158,31 @@ begin
 					training_data(count - 1) <= IrisDataOut; 
 				end if;
 				rst_k <= '1';
+				rst_c <= '1';
 				
 				
 			elsif state(1) = '1' then 			 --may need to change state for this line. this state is for running through the testing data
 				IrisDataType <= '1'; -- run through testing data	
-				rst_k <= '0';
-				if done_k = '1' then
-					wait_count <= wait_count - 1; 
+				rst_k <= '0'; 
+				if done_k = '1' then 
 					if offset_count < 2 then
 						offset_count <= offset_count + 1;
 					else
 						offset_count <= 0;
 					end if;		 
 					mins(offset_count) <= min_dists;
-					rst_k <= '1';
+					rst_k <= '1';  
+					rst_c <= '0';
 				end if;
 				
 				IrisIndex <= to_unsigned(count + 1, 7);
 				test_data <= IrisDataOut;
-				if wait_count = wait_zero then
+				if done_c = '1' then
 					if count < 40 then
 						count <= count + 1;			
 						classifications(count + 1) <= knn_out; -- knn_out is the predicted class of the item. possibly different name;
 					end if;
-					
+					rst_c <= '1';
 				end if;
 			else count <= 0;	
 			end if;	
