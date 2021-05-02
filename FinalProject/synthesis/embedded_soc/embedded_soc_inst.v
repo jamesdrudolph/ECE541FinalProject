d13f54ea9095595f36a3755da999707b3d6cb302
James Rudolph <jrudo004@odu.edu>
2021-05-02 16:55:28 -0400

    synthesis files for new embedded_soc

--- FinalProject/synthesis/embedded_soc/embedded_soc_inst.v
+++ FinalProject/synthesis/embedded_soc/embedded_soc_inst.v
@@ -1,10 +1,10 @@
 	embedded_soc u0 (
+		.buttons_export            (<connected-to-buttons_export>),            //            buttons.export
 		.clk_clk                   (<connected-to-clk_clk>),                   //                clk.clk
 		.led_export                (<connected-to-led_export>),                //                led.export
-		.reset_reset_n             (<connected-to-reset_reset_n>),             //              reset.reset_n
-		.switches_export           (<connected-to-switches_export>),           //           switches.export
-		.buttons_export            (<connected-to-buttons_export>),            //            buttons.export
 		.nios_input_export_export  (<connected-to-nios_input_export_export>),  //  nios_input_export.export
-		.nios_output_export_export (<connected-to-nios_output_export_export>)  // nios_output_export.export
+		.nios_output_export_export (<connected-to-nios_output_export_export>), // nios_output_export.export
+		.reset_reset_n             (<connected-to-reset_reset_n>),             //              reset.reset_n
+		.switches_export           (<connected-to-switches_export>)            //           switches.export
 	);
 
